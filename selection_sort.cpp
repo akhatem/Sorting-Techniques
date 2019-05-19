600c92af87f77f82b99bff9220c5d5452261d410
Ahmed Kareem <31676293+akhatem@users.noreply.github.com>
2019-05-19 14:45:34 +0200

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,29 @@
+/*
+    Selection sort
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort(int a[], int n){
+    for(int j=0; j<n-1; ++j){
+        printf("%d", a[j]);
+        int min=j;
+        for(int i=j+1; i<n; ++i){
+            printf("%d", a[i]);
+            if(a[i]<a[min])
+                min=i;
+        }
+        printf("\n");
+        swap(a[j], a[min]);
+    }
+}
+
+int main() {
+    int n=6;
+    int a[n]={5,4,9,7,1,2};
+    selection_sort(a,n);
+
+    return 0;
+}
